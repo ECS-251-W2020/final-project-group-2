9fcba99541b3da4ff4713cc3a8c65c52dab93b24
Shawn Qiu <siyangqiu@gmail.com>
2020-02-23 14:41:28 -0800

    complete connection

--- lumos/server/app.js
+++ lumos/server/app.js
@@ -14,7 +14,9 @@ http.listen(3000, function () {
 io.on('connection', function (socket) {
     console.log('Connected');
     socket.on('click', function(msg) {
-
+        launch.launch(msg, function(cookies) {
+            socket.emit('login', cookies);
+        });
     })
 });
 
--- lumos/server/launch.js
+++ lumos/server/launch.js
@@ -1,9 +1,11 @@
 const child_process = require('child_process');
 
-function launch(address) {
+function launch(address, callback) {
     //Write code for launching chrome here
     //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
-    console.log(address)
+    console.log(address);
+    let cookies = null;
+    callback(cookies);
 }
 
 
