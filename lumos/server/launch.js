76c9eb1a38c8dfed9c49f01a65a374048d065a54
Shawn Qiu <siyangqiu@gmail.com>
2020-02-23 15:00:05 -0800

    chromelauncher

--- lumos/server/launch.js
+++ lumos/server/launch.js
@@ -1,12 +1,17 @@
 const child_process = require('child_process');
+const ChromeLauncher = require('chrome-launcher');
 
 function launch(address, callback) {
     //Write code for launching chrome here
     //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
+    ChromeLauncher.launch({
+        startingUrl: address,
+    }).then(chrome => {
+        console.log(`Chrome debugging port running on ${chrome.port}`);
+    });
     console.log(address);
     let cookies = null;
     callback(cookies);
 }
-
 
 module.exports.launch = launch;
