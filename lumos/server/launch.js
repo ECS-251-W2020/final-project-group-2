706b18c541a09cf4b6f9e7338ad4f18ca97877d1
Zee Chen <zeyuan.chen1126@gmail.com>
2020-02-23 15:24:27 -0800

    no sandbox

--- lumos/server/launch.js
+++ lumos/server/launch.js
@@ -7,14 +7,14 @@
 // Need to grab the URL and sign-in button
 const puppeteer = require('puppeteer');
 const child_process = require('child_process');
-launch('https://www.google.com').then(() => console.log('Done')).catch(error => console.log(error));
+// launch().then(() => console.log('Done')).catch(error => console.log(error));
 
 async function launch(address, callback) {
   // Create a new browser. By default, the browser is headless,
   // which means it runs in the background and doesn't appear on
   // the screen. Setting `headless: false` opens up a browser
   // window so you can watch what happens.
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({args: ['--no-sandbox']});
 
   //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
   // TODO: make sure address is not malicious?
@@ -25,12 +25,12 @@ async function launch(address, callback) {
   await page.goto(address);
 
 
-//   // Click on login button and wait for the page to load
-//   const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
-//   await page.evaluate(() => {
-//     document.getElementById('signInBtn').click();
-//   });
-//   await waitForLoad;
+  // Click on login button and wait for the page to load
+  const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+  await page.evaluate(() => {
+    document.getElementById('signInBtn').click();
+  });
+  await waitForLoad;
 
   // Create cookies
   let cookies = null;
@@ -43,10 +43,28 @@ async function launch(address, callback) {
 //   await browser.close();
 }
 
-function cookies() {
-    console.log("Create Cookies");
+function cookies(callback) {
+
+    
 }
 
 
 module.exports.launch = launch;
+
+// const ChromeLauncher = require('chrome-launcher');
+
+// function launch(address, callback) {
+//     //Write code for launching chrome here
+//     //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
+//     ChromeLauncher.launch({
+//         startingUrl: address,
+//     }).then(chrome => {
+//         console.log(`Chrome debugging port running on ${chrome.port}`);
+//     });
+//     console.log(address);
+//     let cookies = null;
+//     callback(cookies);
+// }
+
+// module.exports.launch = launch;
 
