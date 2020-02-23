575c511eb3037792f81acfe86fa504dea518eb4d
Zee Chen <zeyuan.chen1126@gmail.com>
2020-02-23 14:41:41 -0800

    add login button listener for launch

--- lumos/server/launch.js
+++ lumos/server/launch.js
@@ -4,6 +4,7 @@
  * Make sure Puppeteer is installed
  */
 
+// Need to grab the URL and sign-in button
 const puppeteer = require('puppeteer');
 run().then(() => console.log('Done')).catch(error => console.log(error));
 
@@ -18,9 +19,20 @@ async function run() {
   const page = await browser.newPage();
   await page.goto('https://google.com');
 
-  // Wait 5 seconds
-  await new Promise(resolve => setTimeout(resolve, 5000));
+//   // Wait 5 seconds
+//   await new Promise(resolve => setTimeout(resolve, 5000));
 
+  // Click on login button and wait for the page to load
+  const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+  await page.evaluate(() => {
+    document.getElementById('signInBtn').click();
+  });
+  await waitForLoad;
+
+  // Create cookies
+  
+
+  // Sends cookie back to VNC
 
 
 
