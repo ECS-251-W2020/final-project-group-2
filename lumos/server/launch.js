f40308010b230082c7528eb817dff985517a0c40
Zee Chen <zeyuan.chen1126@gmail.com>
2020-02-23 16:06:30 -0800

    test 4

--- lumos/server/launch.js
+++ lumos/server/launch.js
@@ -4,49 +4,90 @@
  * Make sure Puppeteer is installed
  */
 
+// const puppeteer = require('puppeteer');
+
 // Need to grab the URL and sign-in button
-const puppeteer = require('puppeteer');
-// const child_process = require('child_process');
+// const puppeteer = require('puppeteer');
+const child_process = require('child_process');
 launch("https://www.google.com").then(() => console.log('Done')).catch(error => console.log(error));
 
 async function launch(address, callback) {
-  // Create a new browser. By default, the browser is headless,
-  // which means it runs in the background and doesn't appear on
-  // the screen. Setting `headless: false` opens up a browser
-  // window so you can watch what happens.
-  const browser = await puppeteer.launch({args: ['--no-sandbox']});
+    // Create a new browser. By default, the browser is headless,
+    // which means it runs in the background and doesn't appear on
+    // the screen. Setting `headless: false` opens up a browser
+    // window so you can watch what happens.
+    const browser = await puppeteer.launch();
+  
+    //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
+    // TODO: make sure address is not malicious?
+    console.log(address);
+  
+    // Open a new page and navigate to passed address
+    const page = await browser.newPage();
+    await page.goto(address);
+  
+  
+    // // Click on login button and wait for the page to load
+    // const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+    // await page.evaluate(() => {
+    //   document.getElementById('signInBtn').click();
+    // });
+    // await waitForLoad;
+  
+    // // Create cookies
+    // let cookies = null;
+    // callback(cookies);
+  
+    // Sends cookie back to VNC
+  
+  
+    // Close the browser and exit the script
+  //   await browser.close();
+  }
+  
+  function cookies(callback) {
+      console.log("No cookies yet");
+      
+  }
+  
+// async function launch(address, callback) {
+//   // Create a new browser. By default, the browser is headless,
+//   // which means it runs in the background and doesn't appear on
+//   // the screen. Setting `headless: false` opens up a browser
+//   // window so you can watch what happens.
+//   const browser = await puppeteer.launch({args: ['--no-sandbox']});
 
-  //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
-  // TODO: make sure address is not malicious?
-  console.log(address);
+//   //"address" is passed in via socket. CANNOT BE TRUSTED TO BE VALID/WELL FORMED. POTENTIALLY MALICIOUS
+//   // TODO: make sure address is not malicious?
+//   console.log(address);
 
-  // Open a new page and navigate to passed address
-  const page = await browser.newPage();
-  await page.goto(address);
-
-
-  // Click on login button and wait for the page to load
-  const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
-  await page.evaluate(() => {
-    document.getElementById('signInBtn').click();
-  });
-  await waitForLoad;
-
-  // Create cookies
-  let cookies = null;
-  callback(cookies);
-
-  // Sends cookie back to VNC
+//   // Open a new page and navigate to passed address
+//   const page = await browser.newPage();
+//   await page.goto(address);
 
 
-  // Close the browser and exit the script
-//   await browser.close();
-}
+//   // Click on login button and wait for the page to load
+//   const waitForLoad = new Promise(resolve => page.on('load', () => resolve()));
+//   await page.evaluate(() => {
+//     document.getElementById('signInBtn').click();
+//   });
+//   await waitForLoad;
 
-function cookies(callback) {
-    console.log("No cookies yet");
+//   // Create cookies
+//   let cookies = null;
+//   callback(cookies);
+
+//   // Sends cookie back to VNC
+
+
+//   // Close the browser and exit the script
+// //   await browser.close();
+// }
+
+// function cookies(callback) {
+//     console.log("No cookies yet");
     
-}
+// }
 
 
 module.exports.launch = launch;
