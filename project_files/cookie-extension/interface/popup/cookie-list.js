711601510d117d52de7f293863e66c3a6a1030e5
suybae@ucdavis.edu <suybae@ucdavis.edu>
2020-02-23 19:11:24 -0800

    import cookies function

--- project_files/cookie-extension/interface/popup/cookie-list.js
+++ project_files/cookie-extension/interface/popup/cookie-list.js
@@ -218,6 +218,8 @@
 
 
 
+
+
     document.getElementById('export-cookies').addEventListener('click', () => {
 
       // let tabUrl = cookieHandler.currentTab.url;
@@ -305,6 +307,22 @@
     document.getElementById('save-create-cookie').addEventListener('click', () => {
       saveCookieForm(document.querySelector('form'));
     });
+
+
+    function importCookies(json) {
+      cookies = JSON.parse(json);
+      cookies.forEach(cookie => {
+        // Make sure we are using the right store ID. This is in case we are importing from a basic store ID and the
+        // current user is using custom containers
+        cookie.storeId = cookieHandler.currentTab.cookieStoreId;
+
+        cookieHandler.saveCookie(cookie, getCurrentTabUrl(), function(error, cookie) {
+          if (error) {
+            sendNotification(error);
+          }
+        });
+      });
+    }
 
     document.getElementById('save-import-cookie').addEventListener('click', e => {
       let buttonIcon = document.getElementById('save-import-cookie').querySelector('use');
