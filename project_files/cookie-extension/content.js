229a70c3fb6a5af80ad989b94300582273cc6be5
suybae@ucdavis.edu <suybae@ucdavis.edu>
2020-02-23 21:09:14 -0800

    nothing

--- project_files/cookie-extension/content.js
+++ project_files/cookie-extension/content.js
@@ -1,9 +1,9 @@
-var elem = document.getElementById('passcode1');
-
-elem.setAttribute("placeholder", "FOOOOOOO");
-
-chrome.tabs.executeScript(null, {
-  file: "interface/popup/cookie-list.js"
-});
-
-console.log(elem);
+// var elem = document.getElementById('passcode1');
+//
+// elem.setAttribute("placeholder", "FOOOOOOO");
+//
+// chrome.tabs.executeScript(null, {
+//   file: "interface/popup/cookie-list.js"
+// });
+//
+// console.log(elem);
