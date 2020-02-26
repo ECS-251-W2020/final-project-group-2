5edaa4ee948747c41d09be7bd8d6bcd81041c901
Haixin Liu <hx@HaixindeMacBook-Pro.local>
2020-02-25 21:12:49 -0800

    combining two extensions functions into one

--- lumos/client/Chrome Extension/background.js
+++ lumos/client/Chrome Extension/background.js
@@ -30,10 +30,58 @@ function exportUrl() {
             value: tabUrl
         }, response => {
             //HAIXIN: call the second extension code here!
-
-
+            
 
         });
+    });
+}
+
+let saveCookie = function (cookie, url) {
+    const newCookie = {
+        domain: cookie.domain || '',
+        name: cookie.name || '',
+        value: cookie.value || '',
+        path: cookie.path || null,
+        secure: cookie.secure || null,
+        httpOnly: cookie.httpOnly || null,
+        expirationDate: cookie.expirationDate || null,
+        storeId: cookie.storeId || this.currentTab.cookieStoreId || null,
+        url: url
+    };
+    if (cookie.hostOnly) {
+        newCookie.domain = null;
+    }
+    chrome.cookies.set(newCookie, (cookieResponse) => {
+        let error = chrome.runtime.lastError;
+        if (!cookieResponse || error) {
+            if (callback) {
+                let errorMessage = (error ? error.message : '') || 'Unknown error';
+                return callback(errorMessage, cookieResponse);
+            }
+            return;
+        }
+    });
+}
+
+function importCookies() {
+    getCurrentTab(function (tabUrl) {
+        fetch(`${url}/${tabUrl}`)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (cookies) {
+                if (cookies.wait === true);
+                else {
+                    cookies.forEach(cookie => {
+                        // Make sure we are using the right store ID. This is in case we are importing from a basic store ID and the
+                        // current user is using custom containers
+                        // cookie.storeId = tabUrl.cookieStoreId;
+
+                        saveCookie(cookie, tabUrl);
+                    });
+
+                }
+            });
     });
 }
 
