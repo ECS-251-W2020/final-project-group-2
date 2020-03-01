8a5c6bcfbfa576567e72d711a58b8674697ed9f8
Zee Chen <zeyuan.chen1126@gmail.com>
2020-03-01 13:15:39 -0800

    change opacity

--- lumos/server/Chrome Extension/content.js
+++ lumos/server/Chrome Extension/content.js
@@ -1,6 +1,6 @@
 // Display UI addition on launch of new browser
 let contentWarning = document.createElement('div');
-contentWarning.style.cssText = 'position:absolute;top:0px;width:100%;height:3%;opacity:0.8;z-index:100;background:#fd9ea9;text-align:center;border-bottom: 1px solid #cacad9;box-shadow: 0 0 10px 0 rgba(0,0,0,.12), 0 2px 3px 0 rgba(0,0,0,.16);';
+contentWarning.style.cssText = 'position:absolute;top:0px;width:100%;height:3%;opacity:1;z-index:100;background:#fd9ea9;text-align:center;border-bottom: 1px solid #cacad9;box-shadow: 0 0 10px 0 rgba(0,0,0,.12), 0 2px 3px 0 rgba(0,0,0,.16);';
 contentWarning.innerHTML += '<div style="text-size: 2em; color: #fff;font-weight: bold;"> Secure Login Page </div>';
 
 document.querySelector('body').appendChild(contentWarning);
