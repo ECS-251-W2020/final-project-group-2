3f5bd3dcbdedf89a5b1ec7a3c5f88da0d4560a7e
Zee Chen <zeyuan.chen1126@gmail.com>
2020-03-01 12:56:52 -0800

    change css for better indicator of secure session

--- lumos/server/Chrome Extension/content.js
+++ lumos/server/Chrome Extension/content.js
@@ -1,6 +1,6 @@
 // Display UI addition on launch of new browser
 let contentWarning = document.createElement('div');
-contentWarning.style.cssText = 'position:absolute;width:100%;height:100%;opacity:0.3;z-index:100;background:#000';
-contentWarning.innerHTML += '<h2> Secure Login Page </h2>';
+contentWarning.style.cssText = 'position:absolute;top:0px;width:100%;height:3%;opacity:0.8;z-index:100;background:#000;text-align:center';
+contentWarning.innerHTML += '<div style="text-size: 2em; color: #fff;"> Secure Login Page </div>';
 
 document.querySelector('body').appendChild(contentWarning);
